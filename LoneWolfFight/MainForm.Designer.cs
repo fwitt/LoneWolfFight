d3a358a0e6017c1e7d2ad8158f7df4cab477b457
Ian <i_r_t@hotmail.com>
2020-07-06 17:28:46 +0100

    Initial commit

---
+++ LoneWolfFight/MainForm.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace LoneWolfFight
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playerStartHpLabel = new System.Windows.Forms.Label();
+            this.playerStartHpNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.enemyStartHpLabel = new System.Windows.Forms.Label();
+            this.enemyStartHpNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.enemySkillNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.enemySkillLabel = new System.Windows.Forms.Label();
+            this.playerSkillNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.playerSkillLabel = new System.Windows.Forms.Label();
+            this.skillDifferenceLabel = new System.Windows.Forms.Label();
+            this.skillDifferenceTextBox = new System.Windows.Forms.TextBox();
+            this.resultsDataGridView = new System.Windows.Forms.DataGridView();
+            this.diceRollColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.playerHpColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.enemyHpColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.enemyEndHpLabel = new System.Windows.Forms.Label();
+            this.playerEndHpLabel = new System.Windows.Forms.Label();
+            this.playerEndHpTextBox = new System.Windows.Forms.TextBox();
+            this.enemyEndHpTextBox = new System.Windows.Forms.TextBox();
+            this.fightButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.playerStartHpNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyStartHpNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemySkillNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerSkillNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playerStartHpLabel
+            // 
+            this.playerStartHpLabel.AutoSize = true;
+            this.playerStartHpLabel.Location = new System.Drawing.Point(13, 13);
+            this.playerStartHpLabel.Name = "playerStartHpLabel";
+            this.playerStartHpLabel.Size = new System.Drawing.Size(58, 15);
+            this.playerStartHpLabel.TabIndex = 0;
+            this.playerStartHpLabel.Text = "Player HP";
+            // 
+            // playerStartHpNumericUpDown
+            // 
+            this.playerStartHpNumericUpDown.Location = new System.Drawing.Point(77, 11);
+            this.playerStartHpNumericUpDown.Name = "playerStartHpNumericUpDown";
+            this.playerStartHpNumericUpDown.Size = new System.Drawing.Size(62, 23);
+            this.playerStartHpNumericUpDown.TabIndex = 1;
+            this.playerStartHpNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.playerStartHpNumericUpDown.ValueChanged += new System.EventHandler(this.StartHpNumericUpDown_ValueChanged);
+            // 
+            // enemyStartHpLabel
+            // 
+            this.enemyStartHpLabel.AutoSize = true;
+            this.enemyStartHpLabel.Location = new System.Drawing.Point(181, 13);
+            this.enemyStartHpLabel.Name = "enemyStartHpLabel";
+            this.enemyStartHpLabel.Size = new System.Drawing.Size(62, 15);
+            this.enemyStartHpLabel.TabIndex = 2;
+            this.enemyStartHpLabel.Text = "Enemy HP";
+            // 
+            // enemyStartHpNumericUpDown
+            // 
+            this.enemyStartHpNumericUpDown.Location = new System.Drawing.Point(249, 11);
+            this.enemyStartHpNumericUpDown.Name = "enemyStartHpNumericUpDown";
+            this.enemyStartHpNumericUpDown.Size = new System.Drawing.Size(62, 23);
+            this.enemyStartHpNumericUpDown.TabIndex = 1;
+            this.enemyStartHpNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.enemyStartHpNumericUpDown.ValueChanged += new System.EventHandler(this.StartHpNumericUpDown_ValueChanged);
+            // 
+            // enemySkillNumericUpDown
+            // 
+            this.enemySkillNumericUpDown.Location = new System.Drawing.Point(249, 40);
+            this.enemySkillNumericUpDown.Name = "enemySkillNumericUpDown";
+            this.enemySkillNumericUpDown.Size = new System.Drawing.Size(62, 23);
+            this.enemySkillNumericUpDown.TabIndex = 1;
+            this.enemySkillNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.enemySkillNumericUpDown.ValueChanged += new System.EventHandler(this.SkillNumericUpDown_ValueChanged);
+            // 
+            // enemySkillLabel
+            // 
+            this.enemySkillLabel.AutoSize = true;
+            this.enemySkillLabel.Location = new System.Drawing.Point(181, 42);
+            this.enemySkillLabel.Name = "enemySkillLabel";
+            this.enemySkillLabel.Size = new System.Drawing.Size(67, 15);
+            this.enemySkillLabel.TabIndex = 2;
+            this.enemySkillLabel.Text = "Enemy Skill";
+            // 
+            // playerSkillNumericUpDown
+            // 
+            this.playerSkillNumericUpDown.Location = new System.Drawing.Point(77, 40);
+            this.playerSkillNumericUpDown.Name = "playerSkillNumericUpDown";
+            this.playerSkillNumericUpDown.Size = new System.Drawing.Size(62, 23);
+            this.playerSkillNumericUpDown.TabIndex = 1;
+            this.playerSkillNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.playerSkillNumericUpDown.ValueChanged += new System.EventHandler(this.SkillNumericUpDown_ValueChanged);
+            // 
+            // playerSkillLabel
+            // 
+            this.playerSkillLabel.AutoSize = true;
+            this.playerSkillLabel.Location = new System.Drawing.Point(13, 42);
+            this.playerSkillLabel.Name = "playerSkillLabel";
+            this.playerSkillLabel.Size = new System.Drawing.Size(63, 15);
+            this.playerSkillLabel.TabIndex = 0;
+            this.playerSkillLabel.Text = "Player Skill";
+            // 
+            // skillDifferenceLabel
+            // 
+            this.skillDifferenceLabel.AutoSize = true;
+            this.skillDifferenceLabel.Location = new System.Drawing.Point(350, 42);
+            this.skillDifferenceLabel.Name = "skillDifferenceLabel";
+            this.skillDifferenceLabel.Size = new System.Drawing.Size(85, 15);
+            this.skillDifferenceLabel.TabIndex = 3;
+            this.skillDifferenceLabel.Text = "Skill Difference";
+            // 
+            // skillDifferenceTextBox
+            // 
+            this.skillDifferenceTextBox.Location = new System.Drawing.Point(438, 39);
+            this.skillDifferenceTextBox.Name = "skillDifferenceTextBox";
+            this.skillDifferenceTextBox.ReadOnly = true;
+            this.skillDifferenceTextBox.Size = new System.Drawing.Size(62, 23);
+            this.skillDifferenceTextBox.TabIndex = 4;
+            this.skillDifferenceTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // resultsListView
+            // 
+            this.resultsDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.diceRollColumn,
+            this.playerHpColumn,
+            this.enemyHpColumn});
+            this.resultsDataGridView.DefaultCellStyle = new System.Windows.Forms.DataGridViewCellStyle { Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight };
+            this.resultsDataGridView.Location = new System.Drawing.Point(13, 77);
+            this.resultsDataGridView.Name = "resultsDataGridView";
+            this.resultsDataGridView.ReadOnly = true;
+            this.resultsDataGridView.RowHeadersVisible = false;
+            this.resultsDataGridView.Size = new System.Drawing.Size(487, 254);
+            this.resultsDataGridView.TabIndex = 5;
+            // 
+            // diceRollColumn
+            // 
+            this.diceRollColumn.Name = "diceRollColumn";
+            this.diceRollColumn.HeaderText = "Roll";
+            this.diceRollColumn.FillWeight = 40;    
+            // 
+            // playerHpColumn
+            // 
+            this.playerHpColumn.Name = "playerHpColumn";
+            this.playerHpColumn.HeaderText = "Player HP";
+            this.playerHpColumn.FillWeight = 40;
+            // 
+            // enemyHpColumn
+            // 
+            this.enemyHpColumn.Name = "enemyHpColumn";
+            this.enemyHpColumn.HeaderText = "Enemy HP";
+            this.enemyHpColumn.FillWeight = 40;
+            // 
+            // enemyEndHpLabel
+            // 
+            this.enemyEndHpLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.enemyEndHpLabel.AutoSize = true;
+            this.enemyEndHpLabel.Location = new System.Drawing.Point(181, 352);
+            this.enemyEndHpLabel.Name = "enemyEndHpLabel";
+            this.enemyEndHpLabel.Size = new System.Drawing.Size(62, 15);
+            this.enemyEndHpLabel.TabIndex = 2;
+            this.enemyEndHpLabel.Text = "Enemy HP";
+            // 
+            // playerEndHpLabel
+            // 
+            this.playerEndHpLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.playerEndHpLabel.AutoSize = true;
+            this.playerEndHpLabel.Location = new System.Drawing.Point(13, 352);
+            this.playerEndHpLabel.Name = "playerEndHpLabel";
+            this.playerEndHpLabel.Size = new System.Drawing.Size(58, 15);
+            this.playerEndHpLabel.TabIndex = 0;
+            this.playerEndHpLabel.Text = "Player HP";
+            // 
+            // playerEndHpTextBox
+            // 
+            this.playerEndHpTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.playerEndHpTextBox.Location = new System.Drawing.Point(77, 349);
+            this.playerEndHpTextBox.Name = "playerEndHpTextBox";
+            this.playerEndHpTextBox.ReadOnly = true;
+            this.playerEndHpTextBox.Size = new System.Drawing.Size(62, 23);
+            this.playerEndHpTextBox.TabIndex = 4;
+            this.playerEndHpTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // enemyEndHpTextBox
+            // 
+            this.enemyEndHpTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.enemyEndHpTextBox.Location = new System.Drawing.Point(249, 349);
+            this.enemyEndHpTextBox.Name = "enemyEndHpTextBox";
+            this.enemyEndHpTextBox.ReadOnly = true;
+            this.enemyEndHpTextBox.Size = new System.Drawing.Size(62, 23);
+            this.enemyEndHpTextBox.TabIndex = 4;
+            this.enemyEndHpTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // fightButton
+            // 
+            this.fightButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.fightButton.Location = new System.Drawing.Point(438, 349);
+            this.fightButton.Name = "fightButton";
+            this.fightButton.Size = new System.Drawing.Size(62, 23);
+            this.fightButton.TabIndex = 6;
+            this.fightButton.Text = "Fight";
+            this.fightButton.UseVisualStyleBackColor = true;
+            this.fightButton.Click += new System.EventHandler(this.FightButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 387);
+            this.Controls.Add(this.fightButton);
+            this.Controls.Add(this.enemyEndHpTextBox);
+            this.Controls.Add(this.playerEndHpTextBox);
+            this.Controls.Add(this.playerEndHpLabel);
+            this.Controls.Add(this.enemyEndHpLabel);
+            this.Controls.Add(this.resultsDataGridView);
+            this.Controls.Add(this.skillDifferenceTextBox);
+            this.Controls.Add(this.skillDifferenceLabel);
+            this.Controls.Add(this.playerSkillLabel);
+            this.Controls.Add(this.playerSkillNumericUpDown);
+            this.Controls.Add(this.enemySkillLabel);
+            this.Controls.Add(this.enemySkillNumericUpDown);
+            this.Controls.Add(this.enemyStartHpNumericUpDown);
+            this.Controls.Add(this.enemyStartHpLabel);
+            this.Controls.Add(this.playerStartHpNumericUpDown);
+            this.Controls.Add(this.playerStartHpLabel);
+            this.MinimumSize = new System.Drawing.Size(532, 426);
+            this.Name = "MainForm";
+            this.Text = "Lone Wolf Fight";
+            ((System.ComponentModel.ISupportInitialize)(this.playerStartHpNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyStartHpNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemySkillNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerSkillNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label playerStartHpLabel;
+        private System.Windows.Forms.NumericUpDown playerStartHpNumericUpDown;
+        private System.Windows.Forms.Label enemyStartHpLabel;
+        private System.Windows.Forms.NumericUpDown enemyStartHpNumericUpDown;
+        private System.Windows.Forms.NumericUpDown enemySkillNumericUpDown;
+        private System.Windows.Forms.Label enemySkillLabel;
+        private System.Windows.Forms.NumericUpDown playerSkillNumericUpDown;
+        private System.Windows.Forms.Label playerSkillLabel;
+        private System.Windows.Forms.Label skillDifferenceLabel;
+        private System.Windows.Forms.TextBox skillDifferenceTextBox;
+        private System.Windows.Forms.DataGridView resultsDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn diceRollColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn playerHpColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn enemyHpColumn;
+        private System.Windows.Forms.Label enemyEndHpLabel;
+        private System.Windows.Forms.Label playerEndHpLabel;
+        private System.Windows.Forms.TextBox playerEndHpTextBox;
+        private System.Windows.Forms.TextBox enemyEndHpTextBox;
+        private System.Windows.Forms.Button fightButton;
+    }
+}
+
---
+++ LoneWolfFight/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoneWolfFight
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private readonly Dictionary<int, Dictionary<int, HpLoss>> SkillDifferenceCharts = new Dictionary<int, Dictionary<int, HpLoss>>
+        {
+            { -6, new Dictionary<int, HpLoss> { { 1, new HpLoss(null, 0) }, { 2, new HpLoss(null, 0) }, { 3, new HpLoss(8, 0) }, { 4, new HpLoss(8, 0) }, { 5, new HpLoss(7, 1) }, { 6, new HpLoss(6, 2) }, { 7, new HpLoss(5, 3) }, { 8, new HpLoss(4, 4) }, { 9, new HpLoss(3, 5) }, { 10, new HpLoss(0, 6) }, } },
+            { -5, new Dictionary<int, HpLoss> { { 1, new HpLoss(null, 0) }, { 2, new HpLoss(8, 0) }, { 3, new HpLoss(7, 0) }, { 4, new HpLoss(7, 1) }, { 5, new HpLoss(6, 2) }, { 6, new HpLoss(6, 3) }, { 7, new HpLoss(5, 4) }, { 8, new HpLoss(4, 5) }, { 9, new HpLoss(3, 6) }, { 10, new HpLoss(0, 7) }, } },
+            { -4, new Dictionary<int, HpLoss> { { 1, new HpLoss(8, 0) }, { 2, new HpLoss(7, 0) }, { 3, new HpLoss(6, 1) }, { 4, new HpLoss(6, 2) }, { 5, new HpLoss(5, 3) }, { 6, new HpLoss(5, 4) }, { 7, new HpLoss(4, 5) }, { 8, new HpLoss(3, 6) }, { 9, new HpLoss(2, 7) }, { 10, new HpLoss(0, 8) }, } },
+            { -3, new Dictionary<int, HpLoss> { { 1, new HpLoss(6, 0) }, { 2, new HpLoss(6, 1) }, { 3, new HpLoss(5, 2) }, { 4, new HpLoss(5, 3) }, { 5, new HpLoss(4, 4) }, { 6, new HpLoss(4, 5) }, { 7, new HpLoss(3, 6) }, { 8, new HpLoss(2, 7) }, { 9, new HpLoss(0, 8) }, { 10, new HpLoss(0, 9) }, } },
+            { -2, new Dictionary<int, HpLoss> { { 1, new HpLoss(6, 1) }, { 2, new HpLoss(5, 2) }, { 3, new HpLoss(5, 3) }, { 4, new HpLoss(4, 4) }, { 5, new HpLoss(4, 5) }, { 6, new HpLoss(3, 6) }, { 7, new HpLoss(2, 7) }, { 8, new HpLoss(1, 8) }, { 9, new HpLoss(0, 9) }, { 10, new HpLoss(0, 10) }, } },
+            { -1, new Dictionary<int, HpLoss> { { 1, new HpLoss(5, 2) }, { 2, new HpLoss(5, 3) }, { 3, new HpLoss(4, 4) }, { 4, new HpLoss(4, 5) }, { 5, new HpLoss(3, 6) }, { 6, new HpLoss(2, 7) }, { 7, new HpLoss(2, 8) }, { 8, new HpLoss(0, 9) }, { 9, new HpLoss(0, 10) }, { 10, new HpLoss(0, 11) }, } },
+            { 0, new Dictionary<int, HpLoss> { { 1, new HpLoss(5, 3) }, { 2, new HpLoss(4, 4) }, { 3, new HpLoss(4, 5) }, { 4, new HpLoss(3, 6) }, { 5, new HpLoss(2, 7) }, { 6, new HpLoss(2, 8) }, { 7, new HpLoss(1, 9) }, { 8, new HpLoss(0, 10) }, { 9, new HpLoss(0, 11) }, { 10, new HpLoss(0, 12) }, } },
+            { 1, new Dictionary<int, HpLoss> { { 1, new HpLoss(5, 4) }, { 2, new HpLoss(4, 5) }, { 3, new HpLoss(3, 6) }, { 4, new HpLoss(3, 7) }, { 5, new HpLoss(2, 8) }, { 6, new HpLoss(2, 9) }, { 7, new HpLoss(1, 10) }, { 8, new HpLoss(0, 11) }, { 9, new HpLoss(0, 12) }, { 10, new HpLoss(0, 14) }, } },
+            { 2, new Dictionary<int, HpLoss> { { 1, new HpLoss(4, 5) }, { 2, new HpLoss(3, 6) }, { 3, new HpLoss(3, 7) }, { 4, new HpLoss(3, 8) }, { 5, new HpLoss(2, 9) }, { 6, new HpLoss(2, 10) }, { 7, new HpLoss(1, 11) }, { 8, new HpLoss(0, 12) }, { 9, new HpLoss(0, 14) }, { 10, new HpLoss(0, 16) }, } },
+            { 3, new Dictionary<int, HpLoss> { { 1, new HpLoss(4, 6) }, { 2, new HpLoss(3, 7) }, { 3, new HpLoss(3, 8) }, { 4, new HpLoss(2, 9) }, { 5, new HpLoss(2, 10) }, { 6, new HpLoss(1, 11) }, { 7, new HpLoss(0, 12) }, { 8, new HpLoss(0, 14) }, { 9, new HpLoss(0, 16) }, { 10, new HpLoss(0, 18) }, } },
+            { 4, new Dictionary<int, HpLoss> { { 1, new HpLoss(4, 7) }, { 2, new HpLoss(3, 8) }, { 3, new HpLoss(2, 9) }, { 4, new HpLoss(2, 10) }, { 5, new HpLoss(2, 11) }, { 6, new HpLoss(1, 12) }, { 7, new HpLoss(0, 14) }, { 8, new HpLoss(0, 16) }, { 9, new HpLoss(0, 18) }, { 10, new HpLoss(0, null) }, } },
+            { 5, new Dictionary<int, HpLoss> { { 1, new HpLoss(3, 8) }, { 2, new HpLoss(3, 9) }, { 3, new HpLoss(2, 10) }, { 4, new HpLoss(2, 11) }, { 5, new HpLoss(2, 12) }, { 6, new HpLoss(1, 14) }, { 7, new HpLoss(0, 16) }, { 8, new HpLoss(0, 18) }, { 9, new HpLoss(0, null) }, { 10, new HpLoss(0, null) }, } },
+            { 6, new Dictionary<int, HpLoss> { { 1, new HpLoss(3, 9) }, { 2, new HpLoss(2, 10) }, { 3, new HpLoss(2, 11) }, { 4, new HpLoss(2, 12) }, { 5, new HpLoss(1, 14) }, { 6, new HpLoss(1, 16) }, { 7, new HpLoss(0, 18) }, { 8, new HpLoss(0, null) }, { 9, new HpLoss(0, null) }, { 10, new HpLoss(0, null) }, } },
+        };
+
+        private void FightButton_Click(object sender, EventArgs e)
+        {
+            this.ClearResults();
+
+            int skillDifferenceFactor = (int)Math.Round((this.playerSkillNumericUpDown.Value - this.enemySkillNumericUpDown.Value) / 2, MidpointRounding.AwayFromZero);
+            skillDifferenceFactor = Math.Min(skillDifferenceFactor, 6);
+            skillDifferenceFactor = Math.Max(skillDifferenceFactor, -6);
+            int playerHp = (int)this.playerStartHpNumericUpDown.Value;
+            int enemyHp = (int)this.enemyStartHpNumericUpDown.Value;
+
+            var skillDifferenceChart = SkillDifferenceCharts[skillDifferenceFactor];
+            var random = new Random();
+
+            while (playerHp > 0 && enemyHp > 0)
+            {
+                var roll = random.Next(1, 11);
+                var hpLoss = skillDifferenceChart[roll];
+                playerHp = (int)(hpLoss.Player == null ? 0 : playerHp - hpLoss.Player);
+                enemyHp = (int)(hpLoss.Enemy == null ? 0 : enemyHp - hpLoss.Enemy);
+
+                this.resultsDataGridView.Rows.Add(roll.ToString(), playerHp.ToString(), enemyHp.ToString());
+            }
+
+            this.playerEndHpTextBox.Text = playerHp.ToString();
+            this.enemyEndHpTextBox.Text = enemyHp.ToString();
+        }
+
+        private void StartHpNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            this.ClearResults();
+        }
+
+        private void ClearResults()
+        {
+            this.resultsDataGridView.Rows.Clear();
+            this.playerEndHpTextBox.Text = string.Empty;
+            this.enemyEndHpTextBox.Text = string.Empty;
+        }
+
+        private void SkillNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            this.skillDifferenceTextBox.Text = (this.playerSkillNumericUpDown.Value - this.enemySkillNumericUpDown.Value).ToString();
+        }
+
+        private class HpLoss
+        {
+            public HpLoss(int? player, int? enemy)
+            {
+                Player = player;
+                Enemy = enemy;
+            }
+
+            public int? Player { get; }
+            public int? Enemy { get; }
+        }
+    }
+}
